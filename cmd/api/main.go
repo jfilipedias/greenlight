92f42243e51cb367e472785aca9b3618fc0c734a
Filipe Dias <filipediascontato@gmail.com>
2024-10-25 09:27:36 -0300

    feat: add  and  resource

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -32,12 +32,9 @@ func main() {
 
 	app := &application{config, logger}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", config.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
